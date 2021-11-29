70f74726320fb5ef4816a07e3a6bbd58eaecc8d4
Tomekxm <tomekxm@onet.pl>
2021-11-29 22:51:06 +0100

    DTO addition

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,25 +2,32 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
-        public AccountController(DataContext context){
+        public AccountController(DataContext context)
+        {
             _context = context;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register (string username, string password){
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.userName)) return BadRequest("Username is already taken");
+
             using var hmac = new HMACSHA512();
 
-            var user = new AppUser{
-                userName = username,
-                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            var user = new AppUser
+            {
+                userName = registerDto.userName.ToLower(),
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.password)),
                 passwordSalt = hmac.Key
             };
 
@@ -31,6 +38,8 @@ namespace API.Controllers
 
         }
 
-
+        private async Task<bool> UserExists(string username) {
+            return await _context.Users.AnyAsync(x => x.userName == username.ToLower());
+        }
     }
 }
---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,8 @@
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+}
