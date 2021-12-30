01fc9adbed8c1e9e7d9e0e4286fa488aa248dccf
Tomekxm <tomekxm@onet.pl>
2021-12-30 13:59:57 +0100

    Unused using directives removal

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -20,7 +17,7 @@ namespace API
             using var scope = host.Services.CreateScope();
             var services = scope.ServiceProvider;
 
-            try 
+            try
             {
                 var context = services.GetRequiredService<DataContext>();
                 await context.Database.MigrateAsync();
@@ -29,9 +26,9 @@ namespace API
             catch (Exception ex)
             {
                 var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(ex,"An error occured during migration");
+                logger.LogError(ex, "An error occured during migration");
             }
-            
+
             await host.RunAsync();
         }
 
