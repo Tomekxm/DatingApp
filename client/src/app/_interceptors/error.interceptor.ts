ff9889f78cdba6276cd24428327d8912c4cb7a71
Tomekxm <tomekxm@onet.pl>
2022-02-09 01:03:05 +0100

    BadRequest message fix

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -29,9 +29,12 @@ export class ErrorInterceptor implements HttpInterceptor {
                   }
                 }
                 throw modalStateErrors.flat();
-              } else {
-                this.toastr.error(error.statusText, error.status);
+              } else if (typeof(error.error === 'object')){
+                this.toastr.error(error.error, error.status);
               }
+                else {
+                  this.toastr.error(error.error, error.status)
+                }
               break;
             case 401:
               this.toastr.error(error.statusText, error.status);
